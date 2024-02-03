953251e10f779fee24ee846926353a87d201a960
Carlos Eduardo <cadu.ce3@gmail.com>
2024-02-03 09:56:47 -0300

    iniciando versão 1 (interação)

---
+++ deteFace/fer/calculator.py
@@ -0,0 +1,27 @@
+happy = 0
+neutral = 24
+surprise = 5
+sad = 0
+angry = 0
+fear = 1
+disgust = 0
+
+def calcularPorcentagem(happy, neutral, surprise, sad, angry, fear, disgust):
+    total = happy + neutral + surprise + sad + angry + fear + disgust
+    happy_percent = (happy * 100) / total
+    neutral_percent = (neutral * 100) / total
+    surprise_percent = (surprise * 100) / total
+    sad_percent = (sad * 100) / total
+    angry_percent = (angry * 100) / total
+    fear_percent = (fear * 100) / total
+    disgust_percent = (disgust * 100) / total
+
+    print(f"Happy: {happy_percent:.2f}%")
+    print(f"Neutral: {neutral_percent:.2f}%")
+    print(f"Surprise: {surprise_percent:.2f}%")
+    print(f"Sad: {sad_percent:.2f}%")
+    print(f"Angry: {angry_percent:.2f}%")
+    print(f"Fear: {fear_percent:.2f}%")
+    print(f"Disgust: {disgust_percent:.2f}%")
+
+calcularPorcentagem(happy, neutral, surprise, sad, angry, fear, disgust)
--- deteFace/fer/v6.py
+++ deteFace/fer/v6.py
@@ -11,11 +11,62 @@ import time
 
 pygame.mixer.init()
 
+# Inicializar o detector de emoções
+detector = FER(mtcnn=True)
+
+# Inicializar a webcam
+cap = cv2.VideoCapture(0)
+
 # Configurar a porta serial
 ser = serial.Serial('COM5', 9600)
 
 # Encontrar o próximo número de arquivo disponível
 file_number = 1
+
+
+base_path = r'C:\Users\caduc\Documents\meuTcc\deteFace\apython-speaks\estudoParte2'
+# audio_inicial = os.path.join(base_path, 'happy', 'entrada1.mp3')
+# som_inicial = pygame.mixer.Sound(audio_inicial)
+# som_inicial.play()
+
+# Função para processar a mensagem e acionar o Arduino
+def processar_mensagem(message):
+    # audio_file = None
+    # base_path = r'C:\Users\caduc\Documents\meuTcc\deteFace\apython-speaks'
+    if message == 'happy':
+        ser.write('0'.encode())
+        #audio_file = os.path.join(base_path, 'happy', 'happy1.mp3')   
+    elif message == 'sad':
+        ser.write('1'.encode())  
+        #audio_file = os.path.join(base_path, 'sad', 'sad1.mp3')  
+    elif message == 'angry':
+        ser.write('2'.encode())
+        #audio_file = os.path.join(base_path, 'angry', 'raiva1.mp3')  
+    elif message == 'fear':
+        ser.write('3'.encode())
+        #audio_file = os.path.join(base_path, 'fear', 'fear1.mp3')  
+    elif message == 'disgust':
+        ser.write('4'.encode())
+        #audio_file = os.path.join(base_path, 'disgust', 'disgust.mp3')  
+    elif message == 'surprise':
+        ser.write('5'.encode())
+        #audio_file = os.path.join(base_path, 'surprise', 'surprise1.mp3')  
+    elif message == 'neutral':
+        ser.write('6'.encode())
+    elif message == 'entrada':
+        ser.write('9'.encode())
+        #audio_file = os.path.join(r'C:\Users\caduc\Documents\meuTcc\deteFace\apython-\neutral', 'happy1.mp3')  
+    else:
+        print("Mensagem não reconhecida")
+
+    # if audio_file is not None and message in ["happy", "sad", "angry", "fear", "disgust", "surprise", "neutral"]:
+    #     sound = pygame.mixer.Sound(audio_file)
+    #     sound.play()
+    #     pygame.time.wait(int(sound.get_length() * 1000))
+
+    #     time.sleep(5)
+
+
 while os.path.exists(f"emotions_output_{file_number}.txt"):
     file_number += 1
 
@@ -26,154 +77,54 @@ output_file = open(output_file_path, "w")
 detected_emotions = {'happy': 0, 'sad': 0, 'angry': 0, 'surprise': 0, 'neutral': 0, 'fear': 0, 'disgust': 0}
 total_frames = 0
 
-def processar_mensagem(message):
+while True:
+    # Ler um frame da webcam
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Detectar emoções no frame
+    emotions = detector.detect_emotions(frame)
+
+    if emotions:
+        total_frames += 1
+        for face in emotions:
+            x, y, w, h = face['box']
+            emotion = max(face['emotions'], key=face['emotions'].get)
+            detected_emotions[emotion] += face['emotions'][emotion]
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+            # Salvar a taxa de emoção no arquivo de texto
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            output_line = f"{timestamp}: {emotion} - {face['emotions'][emotion]}\n"
+            output_file.write(output_line)
+
+            # Processar a mensagem e acionar o Arduino
+            processar_mensagem(emotion)
+
     
-    if message == "1":
-        # print("O usuário acertou")
-        
-        if detected_emotion == 'angry':
-            ser.write('2'.encode())
-             
-    
-    elif message == "0":
-        # print("O roboldo errou")
-        
-        if detected_emotion == 'sad':
-            ser.write('1'.encode())
-        
-    
-    elif message == "2":
-        # print("O roboldo acertou")
-        
-        if detected_emotion == 'happy':
-            ser.write('0'.encode())
-    
-    elif message == "3":
-        # print("O jogador venceu")
-        
-        if detected_emotion == 'sad':
-            ser.write('1'.encode())
-    
-    elif message == "4":
-         # print("O Roboldo venceu")
-        
-        if detected_emotion == 'happy':
-            ser.write('0'.encode())
-    
-    elif message == "5":
-         # print("EMPATE")
-        
-        if detected_emotion == 'surprise':
-            ser.write('5'.encode())
-    
-    elif message == 'inicio':
-        ser.write('6'.encode())
-        
-    
-    else:
-        print("Mensagem não reconhecida")
 
-    # if message in ["0", "1", "2", "3", "4", "5", "inicio"]:
-    #     sound = pygame.mixer.Sound(audio_file)
-    #     sound.play()
-    #     pygame.time.wait(int(sound.get_length() * 1000))
-    # else:
-    #     print("Mensagem não reconhecida")
+    # Exibir o frame com a detecção de emoção
+    cv2.imshow('Webcam', frame)
+
+    # Parar a execução se a tecla 'q' for pressionada
+    if cv2.waitKey(1) & 0xFF == ord('w'):
+        ser.write("9".encode())
+        audio_final = os.path.join(base_path, 'saida1.mp3')
+        som_final = pygame.mixer.Sound(audio_final)
+        som_final.play()
+        time.sleep(8)
+        break
+
+# Fechar o arquivo de texto
+output_file.close()
+
+# Liberar a webcam e fechar a janela
+cap.release()
+cv2.destroyAllWindows()
 
 
-def detectar_emocoes():
-    # Inicializar o detector de emoções
-    detector = FER(mtcnn=True)
-
-    # Inicializar a webcam
-    cap = cv2.VideoCapture(0)
-
-    # Encontrar o próximo número de arquivo disponível
-    file_number = 1
-    while os.path.exists(f"emotions_output_{file_number}.txt"):
-        file_number += 1
-
-    # Abrir o arquivo de texto para salvar as taxas de emoções
-    output_file_path = f"emotions_output_{file_number}.txt"
-    output_file = open(output_file_path, "w")
-
-    detected_emotions = {'happy': 0, 'sad': 0, 'angry': 0, 'surprise': 0, 'neutral': 0, 'fear': 0, 'disgust': 0}
-    total_frames = 0
-
-    emotion_detected = None
-
-    while True:
-        # Ler um frame da webcam
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        # Detectar emoções no frame
-        emotions = detector.detect_emotions(frame)
-
-        if emotions:
-            total_frames += 1
-            for face in emotions:
-                x, y, w, h = face['box']
-                emotion = max(face['emotions'], key=face['emotions'].get)
-                detected_emotions[emotion] += face['emotions'][emotion]
-
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-
-                # Salvar a taxa de emoção no arquivo de texto
-                timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-                output_line = f"{timestamp}: {emotion} - {face['emotions'][emotion]}\n"
-                output_file.write(output_line)
-
-                # Processar a mensagem e acionar o Arduino
-                emotion_detected = emotion
-            return emotion_detected
-        # Exibir o frame com a detecção de emoção
-        cv2.imshow('Webcam', frame)
-
-        # Parar a execução se a tecla 'q' for pressionada
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Calcular a média das taxas de emoção
-    average_emotions = {emotion: rate / total_frames for emotion, rate in detected_emotions.items()}
-
-    # Salvar a média das taxas de emoção no arquivo de texto
-    output_file.write("\n\nMedia das Taxas de Emocao:\n")
-    for emotion, average_rate in average_emotions.items():
-        output_file.write(f"{emotion}: {average_rate}\n")
-
-    # Fechar o arquivo de texto
-    output_file.close()
-
-    # Liberar a webcam e fechar a janela
-    cap.release()
-    cv2.destroyAllWindows()
-
-    print(f"As taxas de emoções foram salvas em {output_file_path}")
-
-HOST = 'localhost'
-PORT = 3005
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind((HOST, PORT))
-    server_socket.listen()
-
-    print('Aguardando conexões...')
-
-    while True:
-        conn, addr = server_socket.accept()
-        print('Conectado por', addr)
-        with conn:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                message = data.decode('utf-8')
-                print('Recebido:', message)
-                processar_mensagem(message)
-                detected_emotion = detectar_emocoes()
-
-                
+print(f"As taxas de emoções foram salvas em {output_file_path}")
